f90d3ec5c507b531f34f504a33b81ee664b7eb08
JuttaEm <jutta.ruuskanen@gmail.com>
2021-05-14 18:54:22 +0300

    Distance + loop working, timer not yet enabled

--- boot.py
+++ boot.py
@@ -1,3 +1,20 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 import esp
+import wificonf
+import network
+
 esp.osdebug(None)
+
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    if not sta_if.isconnected():
+        print('Connecting to network...')
+        sta_if.connect(wificonf.WIFI_SSID, wificonf.WIFI_PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network config: ', sta_if.ifconfig())
+
+
+do_connect()
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import distance
-distance = distance.get_distance()
-print(distance)
+import machine
+import time
+
+
+#tim1 = machine.Timer(1)
+
+count = 0
+
+
+def increase_counter(tim1):
+    global count
+    count += 1
+    return count
+
+
+# tim1.init(period=3000, mode=machine.Timer.PERIODIC,
+    # callback=lambda t: increase_counter)
+
+try:
+    while True:
+        dist = distance.get_distance()
+        if dist < 50:
+            print(dist)
+            count += 1
+            print(count)
+        time.sleep(4)
+except KeyboardInterrupt:
+    pass
