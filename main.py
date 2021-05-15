cc5da5ca58ee4e63ed2fd6995a57a8ed197110d7
JuttaEm <jutta.ruuskanen@gmail.com>
2021-05-15 19:35:53 +0300

    Fixed minor bugs with if-else-statement

--- main.py
+++ main.py
@@ -15,25 +15,26 @@ try:
     while True:
         dist = distance.get_distance()
         dist_str = str(dist)
-        if dist < 50:
+        if ((dist >= 10) and (dist <= 50)):
             print(dist_str)
             count += 1
             print(count)
             sensordata = {'value1': dist_str[:-3], 'value2': count}
             print(sensordata)
-
             request_headers = {'Content-Type': 'application/json'}
-
             request = urequests.post(
                 'http://maker.ifttt.com/trigger/dog_is_drinking/with/key/' + key,
                 json=sensordata,
                 headers=request_headers
             )
-
             print(request.text)
             request.close()
+        else:
+            pass
 
-            time.sleep(4)
+        time.sleep(8)
 
-except OSError as e:
-    print('Failed to read/publish sensor readings')
+except KeyboardInterrupt:
+    pass
+# except OSError as e:
+    #print('Failed to read/publish sensor readings')
